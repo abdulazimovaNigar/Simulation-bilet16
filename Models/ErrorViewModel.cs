4cdcc006d6ca1f7060046e4d23702d2a10088090
abdulazimovNigar <nigarna-mpa201@code.edu.az>
2026-01-13 19:27:15 +0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bilet16Trainers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
